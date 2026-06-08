8e54ecd0e14c03f01419ba6318a60a930a610077
Nicolas Figay <nicolasfigay@macbook-pro-de-nicolas.home>
2026-06-08 08:32:46 +0200

    Hello world ajouté dans index

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-// main.js
 import cytoscape from 'cytoscape'
 import * as d3 from 'd3'
 import i18next from 'i18next'
-import { w2layout, w2popup } from 'w2ui/w2ui-2.0.es6.js'
+import { w2layout } from 'w2ui/w2ui-2.0.es6.js'
 import 'w2ui/w2ui-2.0.min.css'
 
-// Initialisation i18next
 i18next.init({
   lng: 'fr',
   resources: {
-    fr: { translation: { welcome: 'Bienvenue' } }
+    fr: {
+      translation: {
+        welcome: 'Bienvenue'
+      }
+    }
   }
+}).then(() => {
+  document.getElementById('app').innerHTML = `
+    <h1>${i18next.t('welcome')}</h1>
+  `
 })
